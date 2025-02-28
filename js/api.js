57b653fe2d1a4fce8ddeb49717947354f4585735
Jhonatan Oliveira <jhonatanvelaskii@icloud.com>
2025-02-27 21:21:19 -0300

    Adicionando novos estilos a pagina principal.

--- js/api.js
+++ js/api.js
@@ -1,149 +1,140 @@
-import { inject } from "@vercel/analytics"
-import { injectSpeedInsights } from '@vercel/speed-insights';
+// import { inject } from "@vercel/analytics"
+// import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 const apiKey = '8167cc74';
-const frmPesquisa = document.querySelector(".search-box");
+const formPesquisa = document.querySelector("#search-box");
 
 function resetForm() {
-  document.getElementsByClassName(".search-box").reset();
+  formPesquisa.reset();
 }
 
-frmPesquisa.onsubmit = (ev) => {
-  ev.preventDefault();
+formPesquisa.onsubmit = (ev) => {
+  ev.preventDefault(); // Evita o carregamento padrão da página
 
-  const pesquisa = ev.target.pesquisa.value;
+  const pesquisa = ev.target.pesquisa.value; // Obtendo o valor da pesquisa feita pelo usuário
 
-  if (pesquisa == "") {
+  if (pesquisa === "") {
     alert('Preencha o campo!');
     return;
   }
 
+  // Fazer a requisição na API
   fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apiKey}`)
     .then(result => result.json())
     .then(json => carregaLista(json));
 }
 
+// Função para carregar a lista de filmes
 const carregaLista = (json) => {
-  const lista = document.querySelector("div.list");
-  console.log(json)
-  lista.innerHTML = "";
+  const lista = document.querySelector("#list");
+  lista.innerHTML = ""; // Isso limpa a lista 
 
-  if (json.Response == 'False') {
+  if (json.Response === 'False') {
     alert('Nenhum filme encontrado.');
     return;
   }
 
   json.Search.forEach(element => {
-    console.log(element);
-
     let item = document.createElement("div");
     item.classList.add("item");
 
-    item.innerHTML = `<img src="${element.Poster}" /> <h2>${element.Title}</h2>`
+    item.innerHTML = `<img src="${element.Poster}" /> <h2>${element.Title}</h2>`;
 
     lista.appendChild(item);
   });
 }
 
-// Página movies 
-const lista_filmes_destaques = ["The+Shawshank+Redemption", "The+Godfather", "The+Dark+Knight", "The+Godfather+Part+II", "12+Angry+Men", "Schindler's+List", "The+Lord+of+the+Rings%3A+The+Return+of+the+King", "Pulp+Fiction", "The+Lord+of+the+Rings%3A+The+Fellowship+of+the+Ring", "The+Good%2C+the+Bad+and+the+Ugly", "Forrest+Gump", "Fight+Club", "The+Lord+of+the+Rings%3A+The+Two+Towers", "Inception", "Star+Wars%3A+Episode+V+-+The+Empire+Strikes+Back", "The+Matrix", "Goodfellas", "One+Flew+Over+the+Cuckoo's+Nest", "Se7en", "It's+a+Wonderful+Life", "Interstellar", "Seven+Samurai", "The+Silence+of+the+Lambs", "Saving+Private+Ryan", "City+of+God", "Life+Is+Beautiful", "The+Green+Mile", "Terminator+2%3A+Judgment+Day", "Star+Wars%3A+Episode+IV+-+A+New+Hope", "Spider-Man%3A+Across+the+Spider-Verse", "Back+to+the+Future", "Spirited+Away", "The+Pianist", "Parasite", "Psycho", "Gladiator", "The+Lion+King"]
-const lista_series_destaques = ["Breaking+Bad", "Planet+Earth+II", "Planet+Earth", "Band+of+Brothers", "Chernobyl", "The+Wire", "Avatar%3A+The+Last+Airbender", "Blue+Planet+II", "The+Sopranos", "Cosmos%3A+A+Spacetime+Odyssey", "Cosmos", "Our+Planet", "Game+of+Thrones", "The+World+at+War", "Bluey", "Rick+and+Morty", "Fullmetal+Alchemist%3A+Brotherhood", "Life", "The+Last+Dance", "The+Twilight+Zone", "Sherlock", "The+Vietnam+War", "Batman%3A+The+Animated+Series", "Attack+on+Titan", "Scam+1992%3A+The+Harshad+Mehta+Story", "The+Office", "Arcane", "The+Blue+Planet", "Better+Call+Saul", "Human+Planet", "Firefly", "Frozen+Planet", "Clarkson's+Farm", "Hunter+x+Hunter", "Death+Note", "Only+Fools+and+Horses", "The+Civil+War", "True+Detective", "Seinfeld", "The+Decalogue"]
-const lista_filmes_2023 = ["Saltburn", "Poor Things", "Killers+of+the+Flower+Moon", "Society+of+the+Snow", "Oppenheimer", "The Holdovers", "12th Fail", "Anyone But You", "Self Reliance", "Napoleon", "Wonka", "Barbie", "The+Iron+Claw", "Leave+the+World+Behind", "Rebel+Moon%3A+Part+One+-+A+Child+of+Fire", "The+Boys+in+the+Boat", "The Marvels", "The Creator", "Anatomy of a Fall", "Role Play", "I.S.S.", "Aquaman and the Lost Kingdom", "No Hard Feelings", "Animal", "The+Hunger+Games%3A+The+Ballad+of+Songbirds+%26+Snakes", "All of Us Strangers", "Godzilla Minus One", "Maestro", "The Boy and the Heron", "The Kitchen", "The Zone of Interest", "The Color Purple", "Past Lives", "May December", "American Fiction", "Priscilla", "Foe"]
-const lista_series_2023 = ["The+Last+of+Us", "Blue+Eye+Samurai", "Beef", "The+Fall+of+the+House+of+Usher", "The+Artful+Dodger", "The+Traitors", "Percy+Jackson+and+the+Olympians", "The+Curse", "Monarch%3A+Legacy+of+Monsters", "Berlin", "My+Life+with+the+Walter+Boys", "Obliterated", "Echo", "The+Double+Life+of+My+Billionaire+Husband", "Severance", "The+Bear", "House+of+the+Dragon", "Reacher", "Slow+Horses", "The+Gilded+Age", "Halo", "The+Tourist", "Invincible", "Only+Murders+in+the+Building", "The+White+Lotus", "Yellowjackets", "Dr.+Death", "What+If...%3F", "Ted+Lasso", "Jujutsu+Kaisen", "The+Stranger", "The+Boys", "Euphoria", "The+Morning+Show", "For+All+Mankind", "Hazbin+Hotel", "Succession"]
+// Função de randomização para seleção de filmes e séries
 function randomico_inteiro(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === 'Escape') { // Tecla Esc
+    const lista = document.querySelector("#list");
+    if (lista) {
+      lista.innerHTML = ""; // Limpa a lista
+    }
+  }
+});
+
 function carregarImagemInicial() {
-  let lista_filmes_destaques_intime = lista_filmes_destaques.slice(); // Crie uma cópia para evitar modificar o array original
-  let lista_series_destaques_intime = lista_series_destaques.slice()
-  let lista_melhores_filmes_intime = lista_filmes_2023.slice()
-  let lista_melhores_series_intime = lista_series_2023.slice()
-  let index_filme_escolhido, filme_escolhido, index_serie_escolhida, serie_escolhida, index_melhor_filme, melhor_filme_escolhido, index_melhor_serie, melhor_serie_escolhida;
+  let lista_filmes_destaques_intime = [...lista_filmes_destaques];
+  let lista_series_destaques_intime = [...lista_series_destaques];
+  let lista_melhores_filmes_intime = [...lista_filmes_2023];
+  let lista_melhores_series_intime = [...lista_series_2023];
+  
   let filmes_escolhidos = [];
-  for (let i = 1; i < 25; i += 1) {
+  let index_filme_escolhido, filme_escolhido, index_serie_escolhida, serie_escolhida;
+  
+  // Selecionando filmes e séries aleatoriamente e exibindo
+  for (let i = 1; i < 25; i++) {
     index_filme_escolhido = randomico_inteiro(0, lista_filmes_destaques_intime.length - 1);
-    filme_escolhido = lista_filmes_destaques_intime[index_filme_escolhido];
+    filme_escolhido = lista_filmes_destaques_intime.splice(index_filme_escolhido, 1)[0];  // Remove e seleciona
     filmes_escolhidos.push(filme_escolhido);
-    console.log(filme_escolhido)
-    lista_filmes_destaques_intime.splice(index_filme_escolhido, 1); // Remove o filme escolhido do array
     fetch(`https://www.omdbapi.com/?t=${filme_escolhido}&apikey=${apiKey}`)
       .then(result => result.json())
       .then(json => exibirImagemInicial(json, i));
   }
-  for (let i = 25; i < 49; i += 1) {
+
+  for (let i = 25; i < 49; i++) {
     index_serie_escolhida = randomico_inteiro(0, lista_series_destaques_intime.length - 1);
-    serie_escolhida = lista_series_destaques_intime[index_serie_escolhida];
+    serie_escolhida = lista_series_destaques_intime.splice(index_serie_escolhida, 1)[0]; // Remove e seleciona
     filmes_escolhidos.push(serie_escolhida);
-    lista_series_destaques_intime.splice(index_serie_escolhida, 1); // Remove o filme escolhido do array
     fetch(`https://www.omdbapi.com/?t=${serie_escolhida}&apikey=${apiKey}`)
       .then(result => result.json())
       .then(json => exibirImagemInicial(json, i));
   }
-  for (let i = 49; i < 73; i += 1) {
-    index_melhor_filme = randomico_inteiro(0, lista_melhores_filmes_intime.length - 1);
-    melhor_filme_escolhido = lista_melhores_filmes_intime[index_melhor_filme];
-    filmes_escolhidos.push(melhor_filme_escolhido);
-    lista_melhores_filmes_intime.splice(index_melhor_filme, 1); // Remove o filme escolhido do array
-    fetch(`https://www.omdbapi.com/?t=${melhor_filme_escolhido}&apikey=${apiKey}`)
-      .then(result => result.json())
-      .then(json => exibirImagemInicial(json, i));
-  }
-  for (let i = 73; i < 97; i += 1) {
-    index_melhor_serie = randomico_inteiro(0, lista_melhores_series_intime.length - 1);
-    melhor_filme_escolhido = lista_melhores_series_intime[index_melhor_serie];
-    filmes_escolhidos.push(melhor_filme_escolhido);
-    lista_melhores_series_intime.splice(index_melhor_serie, 1); // Remove o filme escolhido do array
-    fetch(`https://www.omdbapi.com/?t=${melhor_filme_escolhido}&apikey=${apiKey}`)
-      .then(result => result.json())
-      .then(json => exibirImagemInicial(json, i));
-  }
+
   return filmes_escolhidos;
 }
 
-// Função para exibir a imagem do primeiro filme
+// Função para exibir a imagem inicial
 function exibirImagemInicial(json, index) {
   if (json.Response === 'False') {
     console.error('Erro na resposta da API ao carregar a imagem inicial.');
     return;
   }
 
-
   let primeiroFilme = json;
+  let imagemInicial = document.querySelector(".pag-movies-imagem-" + index);
+  imagemInicial.innerHTML = `<img src="${primeiroFilme.Poster}" alt="${primeiroFilme.Title}" width="100%">`;
 
-  if (primeiroFilme) {
-    let imagemInicial = document.querySelector(".pag-movies-imagem-" + index);
-    imagemInicial.innerHTML = `<img src="${primeiroFilme.Poster}" alt="${primeiroFilme.Title}" width="100%">`;
-    let nomesInicial = document.getElementById("lista-de-films" + index);
-    nomesInicial.innerHTML = `<li class="list-group-item list-group-item1" style="padding: 0px 0px 0px 0px; border-bottom: 2px solid orange">${primeiroFilme.Title}</li><li class="list-group-item list-group-item2" style="padding-top: 5px"><div>Rating: ${primeiroFilme.imdbRating}</div><div class="barra-avaliacao" ><div class="barra-avaliacaoo" id="barraAvaliacao-${index}"></div><div></li>`;
+  let nomesInicial = document.getElementById("lista-de-films" + index);
+  nomesInicial.innerHTML = `
+    <li class="list-group-item list-group-item1" style="padding: 0px 0px 0px 0px; border-bottom: 2px solid orange">
+      ${primeiroFilme.Title}
+    </li>
+    <li class="list-group-item list-group-item2" style="padding-top: 5px">
+      <div>Rating: ${primeiroFilme.imdbRating}</div>
+      <div class="barra-avaliacao">
+        <div class="barra-avaliacaoo" id="barraAvaliacao-${index}"></div>
+      </div>
+    </li>
+  `;
 
-    function criarBarraAvaliacao(avaliacao) {
-      let escala05 = (avaliacao / 10) * 100;
-      let percentagem = Math.min(Math.max(escala05, 0), 100);
-      return `${percentagem}%`;
-    }
-    let avaliacao = primeiroFilme.imdbRating;
-    document.getElementById('barraAvaliacao-' + index).style.width = criarBarraAvaliacao(avaliacao);
-  } else {
-    console.error('Nenhum filme encontrado na resposta da API ao carregar a imagem inicial.');
+  // Função para criar barra de avaliação
+  function criarBarraAvaliacao(avaliacao) {
+    let escala05 = (avaliacao / 10) * 100;
+    return `${Math.min(Math.max(escala05, 0), 100)}%`;
   }
+
+  document.getElementById('barraAvaliacao-' + index).style.width = criarBarraAvaliacao(primeiroFilme.imdbRating);
 }
 
-// Chama a função para carregar a imagem inicial ao abrir a página  
-var filmes_escolhidoss = carregarImagemInicial();
+// Função de tradução
 function f_traduzir(textToTranslate, callback) {
   const fromLanguage = 'en';
   const toLanguage = 'pt';
 
-  // Construir o URL da requisição
   const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(textToTranslate)}&langpair=${fromLanguage}|${toLanguage}`;
 
-  // Fazer a requisição HTTP
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // A resposta contém a tradução
       const translation = data.responseData.translatedText;
       callback(null, translation);
     })
@@ -153,38 +144,59 @@ function f_traduzir(textToTranslate, callback) {
     });
 }
 
+// Função para mostrar as informações detalhadas do filme ou série
 function showChild(childId) {
   let childDiv = document.getElementById("child");
   childDiv.style.display = "block";
-
-  function showChildInfos(json, filmes) {
-    let fotoChild = document.getElementById("child-foto");
-    let infoChild = document.getElementById("child-conteudo--ul");
-
-    if (json.Type == "movie") {
-      fotoChild.innerHTML = `<img src="${json.Poster}" alt="${json.Title}" width="100%">`;
-      infoChild.innerHTML = `<li><span class="tipo-child">Nome:</span>  ${json.Title}.</li><li><span class="tipo-child">Data de lançamento:</span> ${json.Released}.</li><li><span class="tipo-child">Duração do filme:</span> ${json.Runtime}.</li><li><span class="tipo-child">Rating MetaCritic/Imdb:</span> ${json.Metascore}/100. | ${json.imdbRating}/10.0.</li><li><span class="tipo-child">Bilheteria em dólares (U$):</span> ${json.BoxOffice}.</li><li><span class="tipo-child">Premiações:</span> ${json.Awards}.</li><li><span class="tipo-child">Diretor:</span> ${json.Director}.</li>`;
-    } else if (json.Type == "series") {
-      f_traduzir(json.Plot, function (error, resposta) {
-        if (!error) {
-          fotoChild.innerHTML = `<img src="${json.Poster}" alt="${json.Title}" width="100%">`;
-          infoChild.innerHTML = `<li><span class="tipo-child">Nome:</span>  ${json.Title}.</li><li><span class="tipo-child">Data de lançamento:</span> ${json.Released}.</li><li><span class="tipo-child">Rating Imdb:</span>  ${json.imdbRating}/10.0.</li><li><span class="tipo-child">Total de temporadas:</span> ${json.totalSeasons}.</li><li><span class="tipo-child">Gênero:</span> ${json.Genre}.</li><li><span class="tipo-child">Plot da série:</span> ${resposta}.</li>`;
-        } else {
-          console.error('Erro na tradução do Plot:', error);
-        }
-      });
-    }
-  }
 
   fetch(`https://www.omdbapi.com/?t=${filmes_escolhidoss[String(parseInt(childId) - 1)]}&apikey=${apiKey}`)
     .then(result => result.json())
     .then(json => showChildInfos(json));
 }
 
+// Exibir as informações do filme ou série
+function showChildInfos(json) {
+  let fotoChild = document.getElementById("child-foto");
+  let infoChild = document.getElementById("child-conteudo--ul");
+
+  if (json.Type === "movie") {
+    fotoChild.innerHTML = `<img src="${json.Poster}" alt="${json.Title}" width="100%">`;
+    infoChild.innerHTML = `
+      <li><span class="tipo-child">Nome:</span>  ${json.Title}.</li>
+      <li><span class="tipo-child">Data de lançamento:</span> ${json.Released}.</li>
+      <li><span class="tipo-child">Duração do filme:</span> ${json.Runtime}.</li>
+      <li><span class="tipo-child">Rating MetaCritic/Imdb:</span> ${json.Metascore}/100. | ${json.imdbRating}/10.0.</li>
+      <li><span class="tipo-child">Bilheteira em dólares (U$):</span> ${json.BoxOffice}.</li>
+      <li><span class="tipo-child">Premiações:</span> ${json.Awards}.</li>
+      <li><span class="tipo-child">Diretor:</span> ${json.Director}.</li>
+    `;
+  } else if (json.Type === "series") {
+    f_traduzir(json.Plot, function (error, resposta) {
+      if (!error) {
+        fotoChild.innerHTML = `<img src="${json.Poster}" alt="${json.Title}" width="100%">`;
+        infoChild.innerHTML = `
+          <li><span class="tipo-child">Nome:</span>  ${json.Title}.</li>
+          <li><span class="tipo-child">Data de lançamento:</span> ${json.Released}.</li>
+          <li><span class="tipo-child">Rating Imdb:</span>  ${json.imdbRating}/10.0.</li>
+          <li><span class="tipo-child">Total de temporadas:</span> ${json.totalSeasons}.</li>
+          <li><span class="tipo-child">Gênero:</span> ${json.Genre}.</li>
+          <li><span class="tipo-child">Plot da série:</span> ${resposta}.</li>
+        `;
+      } else {
+        console.error('Erro na tradução do Plot:', error);
+      }
+    });
+  }
+}
+
+// Função para esconder as informações detalhadas
 function hideChild() {
   let childDiv = document.getElementById('child');
   childDiv.style.display = "none";
 }
+
+
+
 var btn_artigos = document.getElementById("header-link-artigos"),
   btn_movies = document.getElementById("header-link-movies"),
   btn_news = document.getElementById("header-link-news"),
@@ -243,5 +255,8 @@ window.addEventListener("resize", ajustarLayout);
 // Chama ajustarLayout inicialmente para configurar o layout no carregamento da página
 ajustarLayout();
 
-injectSpeedInsights();
-inject();
+// injectSpeedInsights();
+// inject();
+
+
+
